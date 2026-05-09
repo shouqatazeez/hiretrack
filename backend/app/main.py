8ff6501f552f97ccd5bd6f560d5ad0ef5464adcf
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-05-09 23:40:12 +0530

    Include auth router to activate registration endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from app.routes.auth import router as auth_router
+
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
     return {"message": "HireTrack backend running"}
+
+
+app.include_router(auth_router)
