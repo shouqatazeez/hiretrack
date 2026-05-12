24d4f373a6c3d9eb3151179605e3b83556711ab9
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-05-13 00:47:14 +0530

    create database tables on app startup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from app.core.database import Base, engine
+from app.models.job import JobApplication 
+from app.models.user import User  
 from app.routes.auth import router as auth_router
+from app.routes.job import router as job_router
 from app.routes.login import router as login_router
 
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def create_tables() -> None:
+    Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
@@ -12,4 +21,5 @@ def home():
 
 
 app.include_router(auth_router)
+app.include_router(job_router)
 app.include_router(login_router)
