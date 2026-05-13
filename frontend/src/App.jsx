976646cf48512c627ddcf35b2aa8502ec2a2f1ae
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-05-13 18:17:14 +0530

    remove workspace editor overrides and cleanup imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,6 @@
-import React from 'react'
-import './index.css'
-
 export default function App() {
   return (
-    <div id="app-root">
+    <div  id="app-root">
       <header style={{ padding: 20, borderBottom: '1px solid var(--border)' }}>
         <h1 style={{ margin: 0 }}>HireTrack</h1>
       </header>
